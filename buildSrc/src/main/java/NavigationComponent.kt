9f804705a45596c979ceebced73f2df29a874b29
Daniel Ilievski <daniel.ilievski@echo.com.mk>
2023-03-22 11:28:40 +0100

    Resolved issues after merging P01-T9 into P01-T7

--- buildSrc/src/main/java/NavigationComponent.kt
+++ buildSrc/src/main/java/NavigationComponent.kt
@@ -2,4 +2,5 @@ object NavigationComponent {
     private const val navigationComponentVersion = "2.5.3"
     const val navigationFragmentKtx = "androidx.navigation:navigation-fragment-ktx:${navigationComponentVersion}"
     const val navigationUiKtx = "androidx.navigation:navigation-ui-ktx:${navigationComponentVersion}"
+    const val navigationSafeArgsPlugin = "androidx.navigation:navigation-safe-args-gradle-plugin:${navigationComponentVersion}"
 }
