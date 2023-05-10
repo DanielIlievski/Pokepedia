c5499b456449b4cf4949d3fdffd2a8c8e9537c43
Daniel Ilievski <daniel.ilievski@echo.com.mk>
2023-05-10 15:25:48 +0200

    Created the MyTeam screen layout.

--- app/src/main/java/com/echo/pokepedia/ui/pokemon/myteam/MyTeamFragment.kt
+++ app/src/main/java/com/echo/pokepedia/ui/pokemon/myteam/MyTeamFragment.kt
@@ -1,19 +1,86 @@
 package com.echo.pokepedia.ui.pokemon.myteam
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.echo.pokepedia.R
+import com.echo.pokepedia.databinding.FragmentMyTeamBinding
+import com.echo.pokepedia.ui.BaseFragment
 
-class MyTeamFragment : Fragment() {
+class MyTeamFragment : BaseFragment() {
 
+    // region fragment variables
+    private var _binding: FragmentMyTeamBinding? = null
+    private val binding get() = _binding!!
+    // endregion
+
+    // region fragment methods
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_team, container, false)
+    ): View {
+        _binding = FragmentMyTeamBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListeners()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+    // endregion
+
+    private fun initListeners() {
+        onBtnDelete1Clicked()
+        onBtnDelete2Clicked()
+        onBtnDelete3Clicked()
+        onBtnDelete4Clicked()
+        onBtnDelete5Clicked()
+        onBtnDelete6Clicked()
+    }
+
+    // region initListeners
+    private fun onBtnDelete1Clicked() {
+        binding.btnDelete1.setOnClickListener {
+            binding.imgPokemon1.setImageResource(R.drawable.ic_add_pokemon)
+        }
+    }
+
+    private fun onBtnDelete2Clicked() {
+        binding.btnDelete2.setOnClickListener {
+            binding.imgPokemon2.setImageResource(R.drawable.ic_add_pokemon)
+        }
+    }
+
+    private fun onBtnDelete3Clicked() {
+        binding.btnDelete3.setOnClickListener {
+            binding.imgPokemon3.setImageResource(R.drawable.ic_add_pokemon)
+        }
+    }
+
+    private fun onBtnDelete4Clicked() {
+        binding.btnDelete4.setOnClickListener {
+            binding.imgPokemon4.setImageResource(R.drawable.ic_add_pokemon)
+        }
+    }
+
+    private fun onBtnDelete5Clicked() {
+        binding.btnDelete5.setOnClickListener {
+            binding.imgPokemon5.setImageResource(R.drawable.ic_add_pokemon)
+        }
+    }
+
+    private fun onBtnDelete6Clicked() {
+        binding.btnDelete6.setOnClickListener {
+            binding.imgPokemon6.setImageResource(R.drawable.ic_add_pokemon)
+        }
+    }
+    // endregion
 }
